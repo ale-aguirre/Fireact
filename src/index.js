81679c59fa9bc460cf45b0fcf733b8401f3c94aa
ale-aguirre <ale.aguirre.2222@hotmail.com>
2021-02-16 18:45:35 -0300

    jumbotron & footer done

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { render } from 'react-dom';
+import 'normalize.css';
+import { GlobalStyles } from "./global-styles";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <GlobalStyles />
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
